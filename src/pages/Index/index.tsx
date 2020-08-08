a897f6fe8afa88d155bc990c046bd628d04915ec
peidrao <contatopedrorn@gmail.com>
2020-08-08 19:39:42 -0300

    conexão com api + estilização

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -7,6 +8,9 @@ import { Title, Form, Properties } from './styles';
 interface Address {
   cidade: string;
   estado: string;
+  cep: string;
+  cidade_info: { codigo_ibge: string; area_km2: string };
+  estado_info: { nome: string };
 }
 
 const Index: React.FC = () => {
@@ -24,27 +28,24 @@ const Index: React.FC = () => {
   }, [address]);
 
   async function handleAddAddress(
-    event: FormEvent<HTMLFontElement>
+    event: FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault();
 
     if (!newAddress) return;
 
     try {
-      const response = await api.get<Address>(`/{newAddress}`);
+      const response = await api.get<Address>(`/${newAddress}`);
       const addres = response.data;
-      console.log(addres);
       setAddress([...address, addres]);
       setNewAdress('');
-    } catch (err) {
-      console.log('erro');
-    }
+    } catch (err) {}
   }
 
   return (
     <>
       <Title>Explore cidades pelo CEP</Title>
-      <Form>
+      <Form onSubmit={handleAddAddress}>
         <input
           placeholder="Digite o CEP"
           value={newAddress}
@@ -52,6 +53,23 @@ const Index: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Properties>
+        {address.map(addres => (
+          <Link key={addres.cidade_info.codigo_ibge} to={`/${addres.cep}`}>
+            <h2>{addres.cidade}</h2>
+
+            <div>
+              <h4>Estado: {addres.estado_info.nome}</h4>
+              <ul>
+                <li>Código do IBGE: {addres.cidade_info.codigo_ibge}</li>
+                <li>Área em km²: {addres.cidade_info.area_km2}</li>
+                  <li> CEP: {addres.cep}</li>
+              </ul>
+            </div>
+          </Link>
+        ))}
+      </Properties>
     </>
   );
 };
--- src/pages/Index/styles.ts
+++ src/pages/Index/styles.ts
@@ -2,48 +2,84 @@ import Styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Title = Styled.h1`
-    font-size: 48px;
-    color: #3a3a3a;
-    max-width: 450px;
-    line-height: 56px;
-    margin-top: 80px;
-`;
+        font-size: 48px;
+        color: #3a3a3a;
+        max-width: 450px;
+        line-height: 56px;
+        margin-top: 80px;
+    `;
 
 export const Form = Styled.form`
-    margin-top: 40px;
-    max-width: 700px;
-    display: flex;
+        margin-top: 40px;
+        max-width: 700px;
+        display: flex;
 
-    input {
-        flex: 1;
-        height: 70px;
-        padding: 0 24px;
-        border: 0;
-        font-size: 30px;
-        border-radius: 5px 0 0 5px;
-        color: #3a3a3a;
-        border: 2px solid #fff;
+        input {
+            flex: 1;
+            height: 70px;
+            padding: 0 24px;
+            border: 0;
+            font-size: 30px;
+            border-radius: 5px 0 0 5px;
+            color: #3a3a3a;
+            border: 2px solid #fff;
 
-        &::placeholder {
-            color: #a8a8b3;
-        }
-    }
-
-    button {
-        width: 200px;
-        weight: 70px;
-        background: #04d361;
-        border-radius: 0 5px 5px 0;
-        border: 0;
-        color: #fff;
-        font-weight: bold;
-        transition: background 0.2s;
-
-        &:hover {
-            background: ${shade(0.2, '#04d361')};
+            &::placeholder {
+                color: #a8a8b3;
+            }
         }
 
-    }
+        button {
+            width: 200px;
+            weight: 70px;
+            background: #7475e5;
+            border-radius: 0 5px 5px 0;
+            border: 0;
+            color: #fff;
+            font-weight: bold;
+            transition: background 0.2s;
+
+            &:hover {
+                background: ${shade(0.2, '#7475e5')};
+            }
+
+        }
+    `;
+
+export const Properties = Styled.div`
+        margin-top: 80px;
+        max-width: 700px;
+
+        a {
+            background: #fff;
+            color: #3a3a3a;
+            border-radius: 5px;
+            width: 100%;
+            padding: 24px;
+            display: block;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            transition: transform 0.2s;
+
+            & + a {
+                margin-top: 16px;
+            }
+
+            &:hover {
+                transform: translateX(10px);
+            }
+
+            div {
+                margin:  0 30px;
+                flex: 1;
+                
+                ul {
+                    list-style: none;
+                }
+
+            }
+
+        }
+
 `;
-
-export const Properties = Styled.span``;
