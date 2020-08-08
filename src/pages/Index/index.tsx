8a5e100d3308f6c1a6d8eb37964c5cd5c3db0602
peidrao <contatopedrorn@gmail.com>
2020-08-08 12:59:49 -0300

    api

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 
 import api from '../../services/api';
 
@@ -10,11 +10,46 @@ interface Address {
 }
 
 const Index: React.FC = () => {
+  const [newAddress, setNewAdress] = useState('');
+  const [address, setAddress] = useState<Address[]>(() => {
+    const storageAddress = localStorage.getItem('@Address:address');
+
+    if (storageAddress) return JSON.parse(storageAddress);
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@Address:address', JSON.stringify(address));
+  }, [address]);
+
+  async function handleAddAddress(
+    event: FormEvent<HTMLFontElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newAddress) return;
+
+    try {
+      const response = await api.get<Address>(`/{newAddress}`);
+      const addres = response.data;
+      console.log(addres);
+      setAddress([...address, addres]);
+      setNewAdress('');
+    } catch (err) {
+      console.log('erro');
+    }
+  }
+
   return (
     <>
       <Title>Explore cidades pelo CEP</Title>
       <Form>
-        <input placeholder="Digite o CEP" />
+        <input
+          placeholder="Digite o CEP"
+          value={newAddress}
+          onChange={e => setNewAdress(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
     </>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://api.postmon.com.br/v1/cep/' });
+const api = axios.create({ baseURL: 'https://api.postmon.com.br/v1/cep' });
 
 export default api;
